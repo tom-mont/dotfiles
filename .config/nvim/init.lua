2c9c504ec0eba4256fa62d4a65e49896af5f6b5d
Tom <tom.b.montgomery@gmail.com>
2025-07-31 20:49:46 +0200

    moved to wezterm

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,9 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 require("config.lsp")
+-- Make background transparent
+function Transparent()
+  vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+  vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+end
+Transparent()
